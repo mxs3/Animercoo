8ff13e57f85d851ee7484c28149eb76072a78641
MxS3 <elsanea2019@gmail.com>
2025-07-10 07:21:29 +0300

    Update FaselHD.js

--- FaselHD.js
+++ FaselHD.js
@@ -1,7 +1,14 @@
 async function fetchSearch(url) {
   const res = await fetchv2(url, {
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+      'Accept-Language': 'en-US,en;q=0.5',
+      'Cache-Control': 'no-cache',
+      'Pragma': 'no-cache',
+      'Upgrade-Insecure-Requests': '1',
+      'Referer': 'https://faselhd.cam/',
+      'Connection': 'keep-alive'
     }
   });
   return await res.text();
